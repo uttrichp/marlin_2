93cfcb197b65523b6a34686998c4e8ef1ce0c84e
uttrichp <uttrichp@gmail.com>
2019-11-28 22:18:30 +0100

    chanded probe offset to https://www.thingiverse.com/thing:2316850

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -921,10 +921,9 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
-
+#define NOZZLE_TO_PROBE_OFFSET { 0, -38, 0 }
 // Certain types of probes need to stay away from edges
-#define MIN_PROBE_EDGE 10
+#define MIN_PROBE_EDGE 20
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000

b2c7d04a43c0da71d2bd6abbd96da208d1a6b86d
Patrick Uttrich <uttrichp@gmail.com>
2019-12-09 00:04:04 +0100

    activated babysteps - soma major fixes - my personal RC ;)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1298,11 +1298,11 @@
 // @section homing
 
 // The center of the bed is at (X=0, Y=0)
-//#define BED_CENTER_AT_0,0
+//#define BED_CENTER_AT_0_0
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-#define MANUAL_X_HOME_POS -38
+#define MANUAL_X_HOME_POS -28
 #define MANUAL_Y_HOME_POS 10
 //#define MANUAL_Z_HOME_POS 0
 
